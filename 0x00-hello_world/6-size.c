8c566a673428e7cb36245a9f94b3ff1404028eae
JAmwenya <jnehondo@gmail.com>
2022-09-08 13:33:17 -0700

    task 6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,11 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
 
-	printf("Size of a char: %lu bite(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu bite(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu bite(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long: %lu bite(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu bite(s)\n", (unsigned long)sizeof(e));
-	return (0);
+printf("Size of a char: %d bite(s)\n", sizeof(char));
+printf("Size of an int: %d bite(s)\n", sizeof(int));
+printf("Size of a long int: %d bite(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bite(s)\n", sizeof(long long int));
+printf("Size of a float: %d bite(s)\n", sizeof(float));
+return (0);
 }
