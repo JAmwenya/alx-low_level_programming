28267b7c41d0eba5931b73ecb0c2761792ed3b21
JAmwenya <jnehondo@gmail.com>
2022-09-10 01:29:25 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int ch;
+
+for (ch = '0'; ch < '10'; ch++)
+{
+putchar (ch);
+}
+putchar('\n');
+return (0);
+}
