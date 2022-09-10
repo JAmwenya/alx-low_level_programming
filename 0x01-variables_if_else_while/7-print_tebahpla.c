a4bd7464cea04ece9003f0fb5900cd8b3e90d328
JAmwenya <jnehondo@gmail.com>
2022-09-10 02:23:41 -0700

    seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int j;
+
+for (j = 'z'; j >= 'a'; j--)
+{
+putchar (j);
+}
+putchar('\n');
+return (0);
+}
