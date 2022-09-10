c21d9f5c6a7cfc423bc0fc223eabeca22d739888
JAmwenya <jnehondo@gmail.com>
2022-09-10 02:58:00 -0700

    nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int j;
+
+for (j = 48; j < 58; j++)
+{
+putchar (j);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
