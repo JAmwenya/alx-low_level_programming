eb06d881d92db713f5fda9ca211bf786addaad04
JAmwenya <jnehondo@gmail.com>
2022-09-14 14:41:22 -0700

    eight

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-
 /**
+ * main - check the code
  *
- * * main - check the code
- *
- *
- *
- * * Return: Always 0.
- *
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
-
-	print_alphabet();
-
+	jack_bauer();
 	return (0);
-
 }
