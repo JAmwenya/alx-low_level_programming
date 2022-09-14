cafa523a023b737da5537000efa95d4f2b3ca0ab
JAmwenya <jnehondo@gmail.com>
2022-09-14 11:52:29 -0700

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 	char ch;
 	int i = 0;
 
-while (i < 10)
-{
-	 ch = 'a';
-	while (ch <= 'z')
-{
-	_putchar(ch);
-	ch++;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
 }
 _putchar('\n');
 i++;
