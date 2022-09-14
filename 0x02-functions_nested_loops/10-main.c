b3647c2fa30d3e2ec210b708f7a4d299b40f331d
JAmwenya <jnehondo@gmail.com>
2022-09-14 15:51:09 -0700

    ten

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,23 +1,17 @@
 #include "main.h"
-
-
+#include <stdio.h>
 
 /**
+ * main - check the code
  *
- * * main - check the code
- *
- *
- *
- * * Return: Always 0.
- *
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
 
-	print_alphabet();
+	int n;
 
+	n = add(89, 9);
+	printf("%d\n", n);
 	return (0);
-
 }
