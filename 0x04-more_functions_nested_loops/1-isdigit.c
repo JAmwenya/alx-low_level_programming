2a4bf1a4ffdb51936568b5cd444619f49831780c
JAmwenya <jnehondo@gmail.com>
2022-10-22 18:47:10 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 excluding 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+	while (i >= 0 && <= 9, && != 2 && != 4);
+	putchar ("i\n");
+
+		return (0);
+}
