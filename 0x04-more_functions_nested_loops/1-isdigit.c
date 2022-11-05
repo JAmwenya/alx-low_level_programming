c9cca02dc0c80a82c230c65fbf0cfc8dd0e59d88
JAmwenya <jnehondo@gmail.com>
2022-11-05 06:44:23 -0700

    more functions, nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,11 @@
 #include "main.h"
+
 /**
- * print_most_numbers - prints the numbers, from 0 to 9 excluding 2 and 4
- * Return: Always 0
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
-void print_most_numbers(void)
+int _isdigit(int c)
 {
-	int i;
-	
-	for (i = 0; i < 10; i++)
-	{
-	     if (!= 2 && i != 4)
-	     {
-	_putchar(i + '0');
-	     }
-	}
-	     _putchar('\n');
+	return (c >= '0' && c <= '9');
 }
--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,4 +1,5 @@
-#include main.h
+
+#include "main.h"
 
 #include <stdio.h>
 
@@ -24,11 +25,11 @@ int main(void)
 
     c = '0';
 
-    printf(%c: %dn, c, _isdigit(c));
+    printf("%c: %d\n", c, _isdigit(c));
 
     c = 'a';
 
-    printf(%c: %dn, c, _isdigit(c));
+    printf("%c: %d\n", c, _isdigit(c));
 
     return (0);
 
