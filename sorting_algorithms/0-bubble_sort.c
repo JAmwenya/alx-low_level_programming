58fc65bca8cdffdcce22a3d2a743d224912b60b7
JAmwenya <jnehondo@gmail.com>
2023-12-20 18:24:59 +0000

    sorting_algorithms

---
+++ sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * @bubble_sort - sorts an array in ascending order
+ * @i: number of times the array iterates
+ * @j: position of element to be swapped
+ * @temp: temporary holder for the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ sorting_algorithms/README.md
@@ -0,0 +1,1 @@
+This directory contains implementations of different sorting algorithms and documents on their time complexities in best case, average case and worst case scenarios.
