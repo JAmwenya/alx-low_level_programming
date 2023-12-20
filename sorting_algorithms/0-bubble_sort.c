5fda389f8f998e8e6d90025f2c7b521be6ce0314
JAmwenya <jnehondo@gmail.com>
2023-12-20 19:01:56 +0000

    sorting_algorithms

--- sorting_algorithms/0-bubble_sort.c
+++ sorting_algorithms/0-bubble_sort.c
@@ -9,13 +9,16 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
 	{
-		for (size_t j = 0; j < (size - i - 1); j++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				
