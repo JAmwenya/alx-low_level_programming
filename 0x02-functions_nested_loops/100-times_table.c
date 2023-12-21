78c35a6220851504a55a3f06b316f5f83a7cb32b
JAmwenya <jnehondo@gmail.com>
2023-12-21 13:06:17 +0000

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 'n' times table, starting with 0.
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int n;
+	for 
+	{
+		(n = 0; n < 15; n++);
+		_puchar("%d", n);
+	}
+	{
+		if (n < 0 || n > 15);
+		_putchar(" ");
+	}
+	_putchar("/n");
+	return(n);
+
+
+
+
+}
--- sorting_algorithms/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains implementations of different sorting algorithms and documents on their time complexities in best case, average case and worst case scenarios.
