fd596e4407ac4f967b28ac92903454c48c7b6868
JAmwenya <jnehondo@gmail.com>
2024-03-13 23:00:04 +0000

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,7 +9,9 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	for (size_t i = 0; i < size; i++)
+	size_t i;
+
+	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
 			return (i);
