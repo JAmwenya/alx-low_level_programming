c3913f1172e815afb218b2282dce917c6cd1ce9c
JAmwenya <jnehondo@gmail.com>
2024-03-13 23:03:02 +0000

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 			return (i);
-		printf("value [%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array == NULL)
 			return (-1);
 	}
